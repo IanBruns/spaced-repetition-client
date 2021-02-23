a153b075ef40c7ab25c06b77cba6fec8ac9a272b
Ian Bruns <ian@rbruns.com>
2021-02-23 14:13:53 -0700

    formatting WordBox

--- src/components/WordBox/WordBox.js
+++ src/components/WordBox/WordBox.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import stein from '../../images/stein.png'
+import './WordBox.css'
 
 export default function WordBox(props) {
     return (
         <div className='WordBox'>
+            <img src={stein} alt='' />
             <h4>{props.original}</h4>
             <p>{`correct answer count: ${props.correct_count}`}</p>
             <p>{`incorrect answer count: ${props.incorrect_count}`}</p>
