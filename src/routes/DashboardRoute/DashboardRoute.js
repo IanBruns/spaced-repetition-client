d4bd32dde5fc8c8911e96cf10bf815bed2769b90
Ian Bruns <ian@rbruns.com>
2021-02-23 12:47:16 -0700

    initial text in dash

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import LangApiService from '../../services/lang-api-service';
 
 class DashboardRoute extends Component {
-  componentDidMount() {
+  state = {
+    words: [],
+    language: '',
+    totalCorrect: 0,
+  }
 
+  componentDidMount() {
+    LangApiService.getAllWords()
+      .then(res => {
+        console.log(res);
+        this.setState({
+          words: res.words,
+          language: res.language.name,
+          totalCorrect: res.language.total_score
+        })
+      })
   }
 
   render() {
     return (
-      <section>
-        implement and style me
+      <section className='a'>
+        <h2>Dashboard</h2>
+        <h2>{`Language: ${this.state.language}`}</h2>
+        <p>{`Total correct answers: ${this.state.totalCorrect}`}</p>
+        <Link to='/learn'>
+          <button>
+            Start practicing
+          </button>
+        </Link>
+
+        <h3>Words to practice</h3>
       </section>
     );
   }
