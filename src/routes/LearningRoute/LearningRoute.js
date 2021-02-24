152b406a0de2b48cd063fb43b319a8a1a2c7026a
Ian Bruns <ian@rbruns.com>
2021-02-24 13:59:30 -0700

    first test passed

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import { Input, Label } from '../../components/Form/Form'
+import LangApiService from '../../services/lang-api-service';
 
 class LearningRoute extends Component {
+  state = {
+    head: []
+  }
+
+  componentDidMount() {
+    LangApiService.languageHeadRequest()
+      .then(head => {
+        this.setState({ head });
+      })
+  }
+
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Translate the word:</h2>
+        <form
+          className='LoginForm'>
+          <div>
+            <Label htmlFor='guess-input'>
+              {this.state.head.nextWord}
+            </Label>
+            <br />
+            <Input
+              ref={this.firstInput}
+              id='login-username-input'
+              name='username'
+              required
+            />
+          </div>
+        </form>
       </section>
     );
   }
--- src/services/lang-api-service.js
+++ src/services/lang-api-service.js
@@ -14,6 +14,18 @@ const LangApiService = {
                     : res.json()
             )
     },
+    languageHeadRequest() {
+        return fetch(`${config.API_ENDPOINT}/language/head`, {
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken()}`,
+            },
+        })
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            )
+    },
 }
 
 export default LangApiService;
